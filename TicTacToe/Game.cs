ba0c115d82f30da4fb6ac39915b10ec3b5b49184
Josh Winter <winterjoshr@gmail.com>
2018-02-01 19:58:42 -0500

    basic working game

---
+++ TicTacToe/Game.cs
@@ -0,0 +1,192 @@
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Text;
+
+namespace TicTacToe
+{
+    internal class Game
+    {
+        internal const int PORT = 3005;
+        internal const int NONE = 0;
+        internal const int X = 1;
+        internal const int O = 2;
+
+        TcpClient tcp;
+        BinaryWriter tcpOut;
+        BinaryReader tcpIn;
+        bool hosting;
+
+        internal string OpponentName { get; private set; }
+        internal string MyName { get; private set; }
+        internal bool IsMyTurn { get; private set; }
+        internal byte[] Table { get; private set; }
+
+        internal Game(string name)
+        {
+            MyName = name;
+            tcp = null;
+            Table = new byte[9];
+            for (int i = 0; i < Table.Length; ++i)
+            {
+                Table[i] = NONE;
+            }
+        }
+
+        internal void Disconnect()
+        {
+            tcp.Close();
+        }
+
+        internal bool Connect(string ipaddr)
+        {
+            tcp = new TcpClient(ipaddr, PORT);
+            bool connected = tcp.Connected;
+            if (connected)
+            {
+                tcpOut = new BinaryWriter(tcp.GetStream());
+                tcpIn = new BinaryReader(tcp.GetStream());
+
+                // tell my name
+                SendString(MyName);
+                // get their name
+                OpponentName = GetString();
+                // opponent is hosting
+                hosting = false;
+                IsMyTurn = true;
+            }
+
+            return connected;
+        }
+
+        internal void Listen()
+        {
+            TcpListener listener = new TcpListener(IPAddress.Any, PORT);
+            listener.Start();
+
+            while (true)
+            {
+                if (listener.Pending())
+                {
+                    tcp = listener.AcceptTcpClient();
+                    listener.Stop();
+
+                    tcpOut = new BinaryWriter(tcp.GetStream());
+                    tcpIn = new BinaryReader(tcp.GetStream());
+
+                    // get their name
+                    OpponentName = GetString();
+                    // tell my name
+                    SendString(MyName);
+                    // i am hosting
+                    hosting = true;
+                    IsMyTurn = false;
+                    return;
+                }
+            }
+        }
+
+        internal void MyTurn(int index)
+        {
+            index = index - 1;
+
+            if (Table[index] != NONE)
+            {
+                throw new TicTacToeException("That slot has previously been selected!");
+            }
+
+            // tell the opponent my desired index
+            if (index < 0 || index > 8)
+            {
+                throw new TicTacToeException("<index> must be [0, 8]");
+            }
+
+            Table[index] = X;
+
+            tcpOut.Write((byte)index);
+
+            IsMyTurn = !IsMyTurn;
+        }
+
+        internal void OpponentTurn()
+        {
+            // read the selected index from the opponent
+            byte b = tcpIn.ReadByte();
+            if (b < 0 || b > 8)
+                throw new TicTacToeException("got byte " + b + " from opponent. They are trying to hack you!!!!!");
+
+            Table[b] = O;
+
+            IsMyTurn = !IsMyTurn;
+        }
+
+        internal bool CheckMeWin()
+        {
+            return CheckWin(GetLetter());
+        }
+
+        internal bool CheckOpponentWin()
+        {
+            return CheckWin(GetLetter() == X ? O : X);
+        }
+
+        private bool CheckWin(int letter)
+        {
+            // rows
+            if (Table[6] == letter && Table[7] == letter && Table[8] == letter)
+                return true;
+            if (Table[3] == letter && Table[4] == letter && Table[5] == letter)
+                return true;
+            if (Table[0] == letter && Table[1] == letter && Table[2] == letter)
+                return true;
+
+            // columns
+            if (Table[6] == letter && Table[3] == letter && Table[0] == letter)
+                return true;
+            if (Table[7] == letter && Table[4] == letter && Table[1] == letter)
+                return true;
+            if (Table[8] == letter && Table[5] == letter && Table[2] == letter)
+                return true;
+
+            // diagnals
+            if (Table[6] == letter && Table[4] == letter && Table[2] == letter)
+                return true;
+            if (Table[8] == letter && Table[4] == letter && Table[0] == letter)
+                return true;
+
+            return false;
+        }
+
+        // return X or O
+        private int GetLetter()
+        {
+            return hosting ? X : O;
+        }
+
+        private string GetString()
+        {
+            Int32 len = tcpIn.ReadInt32();
+            byte[] data = tcpIn.ReadBytes(len);
+
+            return Encoding.ASCII.GetString(data);
+        }
+
+        private void SendString(string str)
+        {
+            tcpOut.Write((Int32)str.Length);
+            byte[] data = Encoding.ASCII.GetBytes(str);
+            tcpOut.Write(data);
+        }
+    }
+
+    internal class TicTacToeException : Exception
+    {
+        public TicTacToeException(string msg)
+        {
+            Message = msg;
+        }
+
+        public override string Message { get; }
+    }
+}
---
+++ TicTacToe/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Run();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: " + e.Message);
+            }
+        }
+
+        private static void Run()
+        {
+            Console.Write("Enter your name: ");
+            string myName = Console.ReadLine();
+
+            Game game = new Game(myName);
+
+            Console.Write("Enter an IP Address, or leave blank to listen: ");
+            string ip = Console.ReadLine();
+            if (ip.Length == 0)
+            {
+                Console.WriteLine("Listening...");
+                game.Listen();
+            }
+            else
+            {
+                if (!game.Connect(ip))
+                {
+                    Console.WriteLine("Could not connect to " + ip + " on port " + Game.PORT);
+                    return;
+                }
+            }
+
+            while (Play(game)) ;
+        }
+
+        private static bool Play(Game game)
+        {
+            Draw(game.Table);
+
+            try
+            {
+                if (game.IsMyTurn)
+                {
+                    Console.Write("Select a slot: ");
+                    int slot = int.Parse(Console.ReadLine());
+                    game.MyTurn(slot);
+                }
+                else
+                {
+                    game.OpponentTurn();
+                }
+            }
+            catch (IOException)
+            {
+                Console.WriteLine("\n\nA network error occured");
+                return false;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                return Play(game);
+            }
+
+            return true;
+        }
+
+        private static void Draw(byte[] board)
+        {
+            Console.WriteLine("-------------");
+            Console.WriteLine($"| {GetChar(board[6])} | {GetChar(board[7])} | {GetChar(board[8])} |");
+            Console.WriteLine("-------------");
+            Console.WriteLine($"| {GetChar(board[3])} | {GetChar(board[4])} | {GetChar(board[5])} |");
+            Console.WriteLine("-------------");
+            Console.WriteLine($"| {GetChar(board[0])} | {GetChar(board[1])} | {GetChar(board[2])} |");
+            Console.WriteLine("-------------");
+        }
+
+        private static char GetChar(int letter)
+        {
+            switch (letter)
+            {
+                case Game.O:
+                    return 'O';
+                case Game.X:
+                    return 'X';
+                case Game.NONE:
+                    return ' ';
+            }
+
+            return '?';
+        }
+    }
+}
